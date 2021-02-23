699089f50dd64caefc71acd7a45e48b2470466e3
mtk13722 <jingjing.liu@mediatek.com>
2021-02-23 14:55:49 +0800

    [ALPS05507253] arm64:config:mt6877:disp:config lcm driver
    config lcm driver
    modify fps in lcm driver.
    
    MTK-Commit-Id: fabc1c4c40252dca73b952c9e3e27b732e84c09a
    
    Change-Id: I7a1bdb868b61fa8d6b1bcb64c6514c3a819d3875
    Signed-off-by: mtk13722 <jingjing.liu@mediatek.com>
    CR-Id: ALPS05507253
    Feature: [Module]Display Driver

--- drivers/gpu/drm/panel/panel-alpha-jdi-nt36672c-cphy-vdo.c
+++ drivers/gpu/drm/panel/panel-alpha-jdi-nt36672c-cphy-vdo.c
@@ -825,7 +825,7 @@ static const struct drm_display_mode performance_mode1 = {
 
 #if defined(CONFIG_MTK_PANEL_EXT)
 static struct mtk_panel_params ext_params = {
-	.pll_clk = 369,
+	.pll_clk = 370,
 	.vfp_low_power = 4178,//45hz
 	.cust_esd_check = 0,
 	.esd_check_enable = 1,
@@ -869,7 +869,7 @@ static struct mtk_panel_params ext_params = {
 		.rc_tgt_offset_hi = 3,
 		.rc_tgt_offset_lo = 3,
 		},
-	.data_rate = 738,
+	.data_rate = 740,
 	.dyn_fps = {
 		.switch_en = 1,
 #if HFP_SUPPORT
@@ -895,7 +895,7 @@ static struct mtk_panel_params ext_params = {
 };
 
 static struct mtk_panel_params ext_params_90hz = {
-	.pll_clk = 369,
+	.pll_clk = 370,
 	.vfp_low_power = 2528,//60hz
 	.cust_esd_check = 0,
 	.esd_check_enable = 1,
@@ -940,7 +940,7 @@ static struct mtk_panel_params ext_params_90hz = {
 		.rc_tgt_offset_hi = 3,
 		.rc_tgt_offset_lo = 3,
 		},
-	.data_rate = 738,
+	.data_rate = 740,
 	.dyn_fps = {
 		.switch_en = 1,
 #if HFP_SUPPORT
@@ -966,7 +966,7 @@ static struct mtk_panel_params ext_params_90hz = {
 };
 
 static struct mtk_panel_params ext_params_120hz = {
-	.pll_clk = 369,
+	.pll_clk = 370,
 	.vfp_low_power = 2528,//idle 60hz
 	.cust_esd_check = 0,
 	.esd_check_enable = 1,
@@ -1010,7 +1010,7 @@ static struct mtk_panel_params ext_params_120hz = {
 		.rc_tgt_offset_hi = 3,
 		.rc_tgt_offset_lo = 3,
 		},
-	.data_rate = 738,
+	.data_rate = 740,
 	.dyn_fps = {
 		.switch_en = 1,
 		.vact_timing_fps = 120,
