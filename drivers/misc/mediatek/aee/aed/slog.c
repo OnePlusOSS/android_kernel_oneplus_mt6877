6c1139c43bafb22b6a66488cba3904b2a24edba4
Weiwei.Zhang <Weiwei.Zhang@mediatek.com>
2021-02-23 14:53:05 +0800

    [ALPS05559446] slog: hook ko module
    Add hook ko module, so slog can monitor ko module.
    
    MTK-Commit-Id: 7d7160b564aa77dc5c230dd9435a4deee3eebaa1
    
    Change-Id: I8463c9e667d8f370c9be4f6df5013cc83c871ca9
    Signed-off-by: Weiwei.Zhang <Weiwei.Zhang@mediatek.com>
    CR-Id: ALPS05559446
    Feature: Android Exception Engine(AEE)

--- drivers/misc/mediatek/aee/aed/slog.c
+++ drivers/misc/mediatek/aee/aed/slog.c
@@ -20,13 +20,6 @@
 /**
  * Data structures to store tracepoints information
  */
-struct tracepoints_table {
-	const char *name;
-	void *func;
-	struct tracepoint *tp;
-	bool init;
-};
-
 void slog(const char *fmt, ...)
 {
 	struct va_format vaf;
@@ -39,26 +32,40 @@ void slog(const char *fmt, ...)
 	va_end(args);
 }
 
+struct tracepoints_table {
+	const char *name;
+	const char *mod_name;
+	bool module;
+	void *func;
+	struct tracepoint *tp;
+	bool init;
+};
+
+static unsigned int ufs_count;
+static unsigned int ccci_count;
+
 static void probe_ufs_mtk_event(void *data, unsigned int type,
-				unsigned int val)
+	unsigned int val)
 {
-	slog("#$#ufs#@#event#%lu:%lu(0x%x)", type, val, val);
+	ufs_count++;
+	slog("#$#ufs#@#event#%lu:%lu(0x%x)#%d", type, val, val, ufs_count);
 }
 
-static void __nocfi probe_ccci_event(void *data, char *string, char *sub_string,
+static void probe_ccci_event(void *data, char *string, char *sub_string,
 	unsigned int sub_type, unsigned int resv)
 {
-	slog("#$#%s#@#%s#%d:%d", string, sub_string, sub_type, resv);
+	ccci_count++;
+	slog("#$#%s#@#%s#%d:%d#%d", string, sub_string, sub_type, resv, ccci_count);
 }
 
 static struct tracepoints_table interests[] = {
-	{.name = "ufs_mtk_event", .func = probe_ufs_mtk_event},
-	{.name = "ccci_event", .func = probe_ccci_event},
+	{.name = "ufs_mtk_event", .mod_name = NULL, .module = false, .func = probe_ufs_mtk_event},
+	{.name = "ccci_event", .mod_name = "NULL", .module = false, .func = probe_ccci_event},
 };
 
 #define FOR_EACH_INTEREST(i) \
-	for (i = 0; i < sizeof(interests) / \
-	     sizeof(struct tracepoints_table); i++)
+	for (i = 0; i < sizeof(interests) / sizeof(struct tracepoints_table); \
+	i++)
 
 /**
  * Find the struct tracepoint* associated with a given tracepoint
@@ -74,7 +81,7 @@ static void lookup_tracepoints(struct tracepoint *tp, void *ignore)
 	}
 }
 
-static void mtk_slog_deinit(void)
+static void cleanup(void)
 {
 	int i;
 
@@ -86,44 +93,66 @@ static void mtk_slog_deinit(void)
 	}
 }
 
-static void mtk_slog_init_map(void)
+#ifdef MODULE
+static void for_each_modlist_tracepoint(
+	void (*fct)(struct tracepoint *tp, void *priv),
+	void *priv)
 {
+#if IS_ENABLED(CONFIG_TRACEPOINTS)
+	struct module *mod;
+	tracepoint_ptr_t *iter, *begin, *end;
 	int i;
+	if (!fct)
+		return;
+	for (i = 0; i < ARRAY_SIZE(interests); i++) {
+		if (interests[i].module == true) {
+			mutex_lock(&module_mutex);
+			mod = find_module(interests[i].mod_name);
+			mutex_unlock(&module_mutex);
 
-	/* Install the tracepoints */
-	for_each_kernel_tracepoint(lookup_tracepoints, NULL);
+			if (!mod)
+				continue;
 
-	FOR_EACH_INTEREST(i) {
-		if (interests[i].tp == NULL) {
-			pr_info("Error: %s not found\n", interests[i].name);
-			/* Unload previously loaded */
-			mtk_slog_deinit();
-			return;
+			begin = mod->tracepoints_ptrs;
+			end = mod->tracepoints_ptrs + mod->num_tracepoints;
+			for (iter = begin; iter < end; iter++)
+				fct(tracepoint_ptr_deref(iter), NULL);
 		}
-
-		tracepoint_probe_register(interests[i].tp, interests[i].func,
-					  NULL);
-		interests[i].init = true;
 	}
+#endif
 }
+#endif
 
 static int __init mtk_slog_init(void)
 {
-	mtk_slog_init_map();
+	int i;
+#ifdef MODULE
+	for_each_modlist_tracepoint(lookup_tracepoints, NULL);
+#endif
+	for_each_kernel_tracepoint(lookup_tracepoints, NULL);
 
+	FOR_EACH_INTEREST(i) {
+		for (i = 0; i < ARRAY_SIZE(interests); i++) {
+			if (interests[i].tp == NULL) {
+				pr_info("Error: %s\n", interests[i].name);
+				continue;
+			}
+			tracepoint_probe_register(interests[i].tp, interests[i].func,
+						  NULL);
+
+			interests[i].init = true;
+		}
+	}
 	return 0;
 }
 
 static void __exit mtk_slog_exit(void)
 {
-	mtk_slog_deinit();
+	cleanup();
 }
 
-/*
- * TODO: The timing of loadable module is too late to have full
- * list of kernel threads. Need to find out solution.
- */
-early_initcall(mtk_slog_init);
+module_init(mtk_slog_init);
+module_exit(mtk_slog_exit);
 
 MODULE_AUTHOR("MediaTek Inc.");
 MODULE_LICENSE("GPL");
