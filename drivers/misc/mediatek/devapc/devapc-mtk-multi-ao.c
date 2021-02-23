76ae7a0edc23ba241dfdbd9a41f27ed15a30c7d1
Jackson Chang <jackson-kt.chang@mediatek.com>
2021-02-23 14:55:47 +0800

    [ALPS05560737] devapc: add driver for mt6877
    Add devapc driver for mt6877.
    
    MTK-Commit-Id: 1e1ddfda5c15dbe9b45344959a736d25e0b15fb5
    
    Change-Id: Iab3be1efd798bacd820878aa33733f1aa6d5c473
    Signed-off-by: Jackson Chang <jackson-kt.chang@mediatek.com>
    CR-Id: ALPS05560737
    Feature: [Module]Device APC

--- drivers/misc/mediatek/devapc/devapc-mtk-multi-ao.c
+++ drivers/misc/mediatek/devapc/devapc-mtk-multi-ao.c
@@ -1379,6 +1379,8 @@ int mtk_devapc_probe(struct platform_device *pdev,
 
 	pr_debug(PFX " IRQ:%d\n", mtk_devapc_ctx->devapc_irq);
 
+/* Enable devapc-infra-clock in preloader */
+#ifndef CONFIG_DEVAPC_MT6877
 	/* CCF (Common Clock Framework) */
 	mtk_devapc_ctx->devapc_infra_clk = devm_clk_get(&pdev->dev,
 			"devapc-infra-clock");
@@ -1387,6 +1389,7 @@ int mtk_devapc_probe(struct platform_device *pdev,
 		pr_err(PFX "(Infra) Cannot get devapc clock from CCF\n");
 		return -EINVAL;
 	}
+#endif
 
 	proc_create("devapc_dbg", 0664, NULL, &devapc_dbg_fops);
 
@@ -1398,10 +1401,13 @@ int mtk_devapc_probe(struct platform_device *pdev,
 		pr_info(PFX "create SWP sysfs file failed, ret:%d\n", ret);
 #endif
 
+/* Enable devapc-infra-clock in preloader */
+#ifndef CONFIG_DEVAPC_MT6877
 	if (clk_prepare_enable(mtk_devapc_ctx->devapc_infra_clk)) {
 		pr_err(PFX " Cannot enable devapc clock\n");
 		return -EINVAL;
 	}
+#endif
 
 	start_devapc();
 
@@ -1419,7 +1425,10 @@ EXPORT_SYMBOL_GPL(mtk_devapc_probe);
 
 int mtk_devapc_remove(struct platform_device *dev)
 {
+/* devapc-infra-clock is always on */
+#ifndef CONFIG_DEVAPC_MT6877
 	clk_disable_unprepare(mtk_devapc_ctx->devapc_infra_clk);
+#endif
 	return 0;
 }
 EXPORT_SYMBOL_GPL(mtk_devapc_remove);
