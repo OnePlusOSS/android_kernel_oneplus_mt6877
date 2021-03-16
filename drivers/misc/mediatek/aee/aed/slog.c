bba46ac9200dc8160554a4f2ce2ff23c33a850b9
Weiwei.Zhang <Weiwei.Zhang@mediatek.com>
2021-03-16 19:53:40 +0800

    [ALPS05598014] slog: add /proc/slog/slog_threshold file
    1. add /proc/slog/slog_threshold file
    2. hook GPU hardstop/cache parity kernel API
    3. hook combo_bt/combo_wifi EE
    
    MTK-Commit-Id: 4672d56cfb6c9bfeae696214f978752ceaf2b87e
    
    Change-Id: Ic4eae49996d7e1f7084d3a97e5d4bbeb358b0a87
    Signed-off-by: Weiwei.Zhang <Weiwei.Zhang@mediatek.com>
    CR-Id: ALPS05598014
    Feature: Android Exception Engine(AEE)

--- drivers/misc/mediatek/aee/aed/slog.c
+++ drivers/misc/mediatek/aee/aed/slog.c
@@ -13,13 +13,29 @@
 
 #include <linux/module.h>
 #include <linux/tracepoint.h>
+#include <linux/proc_fs.h>
 
 #define CREATE_TRACE_POINTS
 #include <mt-plat/slog.h>
+#include <mt-plat/aee.h>
 
 /**
  * Data structures to store tracepoints information
  */
+struct tracepoints_table {
+	const char *name;
+	const char *mod_name;
+	bool module;
+	void *func;
+	struct tracepoint *tp;
+	bool init;
+};
+
+static unsigned int ufs_count;
+static unsigned int ccci_count;
+static unsigned int threshold;
+static struct proc_dir_entry *dir;
+
 void slog(const char *fmt, ...)
 {
 	struct va_format vaf;
@@ -32,23 +48,14 @@ void slog(const char *fmt, ...)
 	va_end(args);
 }
 
-struct tracepoints_table {
-	const char *name;
-	const char *mod_name;
-	bool module;
-	void *func;
-	struct tracepoint *tp;
-	bool init;
-};
-
-static unsigned int ufs_count;
-static unsigned int ccci_count;
-
 static void probe_ufs_mtk_event(void *data, unsigned int type,
 	unsigned int val)
 {
 	ufs_count++;
 	slog("#$#ufs#@#event#%lu:%lu(0x%x)#%d", type, val, val, ufs_count);
+	if ((threshold > 0) && ((ufs_count % threshold) == 0)) {
+		aee_kernel_exception("ufs", "occur:%d, threshold:%d\n", ufs_count, threshold);
+	}
 }
 
 static void probe_ccci_event(void *data, char *string, char *sub_string,
@@ -123,6 +130,105 @@ static void for_each_modlist_tracepoint(
 }
 #endif
 
+static int slog_threshold_proc_show(struct seq_file *m, void *v)
+{
+	seq_printf(m, "slog threshold: %d\n", threshold);
+	return 0;
+}
+
+static char *_copy_from_user_for_proc(const char __user *buffer, size_t count)
+{
+	char *buf = (char *)__get_free_page(GFP_USER);
+
+	if (!buf)
+		return NULL;
+
+	if (count >= PAGE_SIZE)
+		goto out;
+
+	if (copy_from_user(buf, buffer, count))
+		goto out;
+
+	buf[count] = '\0';
+
+	return buf;
+
+out:
+	free_page((unsigned long)buf);
+
+	return NULL;
+}
+
+static ssize_t slog_threshold_proc_write(struct file *file,
+	const char __user *buffer, size_t count, loff_t *pos)
+{
+	unsigned int num;
+	int rc;
+
+	char *buf = _copy_from_user_for_proc(buffer, count);
+
+	if (!buf)
+		return -EINVAL;
+
+	rc = kstrtouint(buf, 10, &num);
+
+	if (rc < 0)
+		pr_info("transfer string to int error\n");
+	else
+		threshold = num;
+
+	free_page((unsigned long)buf);
+
+	pr_info("set threshold to %d\n", threshold);
+	return count;
+}
+
+#define PROC_FOPS_RW(name)                                              \
+static int name ## _proc_open(struct inode *inode, struct file *file)	\
+{                                                                       \
+	return single_open(file, name ## _proc_show, PDE_DATA(inode));  \
+}                                                                       \
+static const struct file_operations name ## _proc_fops = {              \
+	.owner          = THIS_MODULE,                                  \
+	.open           = name ## _proc_open,                           \
+	.read           = seq_read,                                     \
+	.llseek         = seq_lseek,                                    \
+	.release        = single_release,                               \
+	.write          = name ## _proc_write,                          \
+}
+
+#define PROC_ENTRY(name)        {__stringify(name), &name ## _proc_fops}
+
+PROC_FOPS_RW(slog_threshold);
+
+int slog_procfs_init(void)
+{
+	int i = 0;
+
+	struct pentry {
+		const char *name;
+		const struct file_operations *fops;
+	};
+
+	const struct pentry entries[] = {
+		PROC_ENTRY(slog_threshold),
+	};
+
+	dir = proc_mkdir("slog", NULL);
+
+	if (!dir) {
+		pr_info("mkdir /proc/slog failed\n");
+		return -ENOMEM;
+	}
+
+	for (i = 0; i < ARRAY_SIZE(entries); i++) {
+		if (!proc_create(entries[i].name, 0640, dir, entries[i].fops))
+			pr_info("create /proc/slog/%s failed\n", entries[i].name);
+	}
+
+	return 0;
+}
+
 static int __init mtk_slog_init(void)
 {
 	int i;
@@ -143,11 +249,13 @@ static int __init mtk_slog_init(void)
 			interests[i].init = true;
 		}
 	}
+	slog_procfs_init();
 	return 0;
 }
 
 static void __exit mtk_slog_exit(void)
 {
+	remove_proc_entry("slog_threshold", dir);
 	cleanup();
 }
 
